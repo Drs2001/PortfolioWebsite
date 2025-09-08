2fb61f05f030a1807f87997a89f85f5e4a5cf0a7
Drs2001 <Dylanspence705607@gmail.com>
2025-09-08 13:59:47 -0400

    added global styling and a navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,12 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
     return (
         <div className={styles.navbar}>
+            <Link href="/" className={styles.link}>Home</Link>
             <Link href="/projects" className={styles.link}>Projects</Link>
+            <Link href="/aboutme" className={styles.link}>About Me</Link>
+            <Link href="/education" className={styles.link}>Education</Link>
+            <Link href="/experience" className={styles.link}>Experience</Link>
+            <Link href="/skills" className={styles.link}>Skills</Link>
         </div>
     )
 }
