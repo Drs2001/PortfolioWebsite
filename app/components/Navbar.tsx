c1c52a172dd1b3d02f14c4a6a87ff1265ec0a196
Drs2001 <Dylanspence705607@gmail.com>
2025-09-09 15:05:39 -0400

    Cleaned up homepage, added socials icon links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,8 +6,8 @@ const Navbar = () => {
     return (
         <div className={styles.navbar}>
             <Link href="/" className={styles.link}>Home</Link>
-            <Link href="/projects" className={styles.link}>Projects</Link>
             <Link href="/aboutme" className={styles.link}>About Me</Link>
+            <Link href="/projects" className={styles.link}>Projects</Link>
             <Link href="/education" className={styles.link}>Education</Link>
             <Link href="/experience" className={styles.link}>Experience</Link>
             <Link href="/skills" className={styles.link}>Skills</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,30 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
+      <div className="pt-60">
+        <p>Hi I am</p>
+        <p className="font-bold text-lg">Dylan Spence</p>
+        <p className="title">Full Stack Developer / IT Enthusiest</p>
+        <div className="links">
+          <a href="https://github.com/Drs2001" target="_blank" rel="noopener noreferrer">
+            <div className="icons">
+              <img src="/github-mark-white.svg" alt="Open link" width="24" height="24"></img>
+            </div>
+          </a>
+          <a href="https://www.linkedin.com/in/dylan-spence-999036234/" target="_blank" rel="noopener noreferrer">
+            <div className="icons">
+              <img src="/linkedin.png" alt="Open link" width="24" height="24"></img>
+            </div>
+          </a>
+          <a href="https://gitea.spence.cloud/Drs2001" target="_blank" rel="noopener noreferrer">
+            <div className="icons">
+              <img src="/gitea.png" alt="Open link" width="24" height="24"></img>
+            </div>
+          </a>
+        </div>
+      </div>
+      <Image src="/headshot.png" width={500} height={500} alt="" />
     </div>
   );
 }
