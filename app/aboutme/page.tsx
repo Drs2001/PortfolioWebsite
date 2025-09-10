98c7c002e633e67d009e1520f5b64aa9c8c0146c
Drs2001 <Dylanspence705607@gmail.com>
2025-09-10 16:45:50 -0400

    Started work on about me section

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import styles from "./page.module.css";
+import data from "../data.json"
 
 const Aboutme = () => {
     return (
-        <div>Aboutme</div>
+        <div className={styles.page}>
+            <div className={styles.header}>
+                <h1 className="title">About Me</h1>
+                <div className={styles.nav}>
+                    <a href="#summary" className={styles.link}>Summary</a>
+                    <a href="#interests" className={styles.link}>Interests/ Hobbies</a>
+                    <a href="#history" className={styles.link}>My History</a>
+                </div>
+            </div>
+            <div className={styles.body}>
+                <h2 id="summary" className={styles.subHeading}>Summary</h2>
+                <p>{data.about.summary}</p>
+                <h2 id="interests" className={styles.subHeading}>Interests/ Hobbies</h2>
+                <p>{data.about.interests}</p>
+                <h2 id="history" className={styles.subHeading}>My History</h2>
+                <p>{data.about.history}</p>
+            </div>
+        </div>
     )
 }
 
