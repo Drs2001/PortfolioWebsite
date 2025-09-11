f6b6f31bb179ffdf5965b954984748bd412ae407
Drs2001 <Dylanspence705607@gmail.com>
2025-09-11 14:56:32 -0400

    updated education page with diploma

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 const Education = () => {
     return (
-        <div>Education</div>
+        <div className={styles.page}>
+            <h1 className="title">
+                Education
+            </h1>
+            <Image src="/diploma.png" width={1000} height={1000} alt="" />
+        </div>
     )
 }
 
