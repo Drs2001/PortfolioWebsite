e4fa7005e01a096c9e3b3d2979c1f181f67145c0
Drs2001 <Dylanspence705607@gmail.com>
2025-09-11 14:42:43 -0400

    Finished skills page

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,17 +1,37 @@
+"use client";
+
+import { useState } from 'react';
 import React from 'react'
 import styles from "./page.module.css";
 import SkillCard from "../components/SkillCard";
 import data from "../data.json";
 
 const Skills = () => {
+    const [query, setQuery] = useState("");
+
+    const filteredSkills = data.skills
+    .filter((skill) =>
+        skill.name.toLowerCase().includes(query.toLowerCase())
+    )
+    .sort((a, b) => 
+        Number(b.percent) - Number(a.percent)
+    );
+    
     return (
         <div className={styles.page}>
             <h1 className="title">Skills</h1>
+            <input 
+                type="text" 
+                placeholder="Search Skills.." 
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                className={styles.searchBar}
+            />
             <div className={styles.cards}>
-                {data.skills.map((skill: any, index: number) => (
+                {filteredSkills.map((skill: any, index: number) => (
                     <SkillCard 
                         key={index}
-                        skill={skill.skill}
+                        skill={skill.name}
                         percent={skill.percent}
                     />
                 ))}
